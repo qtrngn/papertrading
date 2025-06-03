c25ee3ba7c69db600f37f7b00492da4609da5c1c
qtrngn <nguyentrucquynh98@gmail.com>
2025-06-03 13:25:05 -0700

    update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAa5FAO8rTZOhOO3BbmQOAt_GlDrdX-kmA",
-  authDomain: "papertrading-a6c0a.firebaseapp.com",
-  projectId: "papertrading-a6c0a",
-  storageBucket: "papertrading-a6c0a.firebasestorage.app",
-  messagingSenderId: "872200259125",
-  appId: "1:872200259125:web:d0b492e5144cc47b05d37c",
-  measurementId: "G-TH5CSGYRKZ"
+  
 };
 
 const app = initializeApp(firebaseConfig);
